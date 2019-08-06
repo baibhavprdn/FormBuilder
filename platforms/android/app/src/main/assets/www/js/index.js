2e02ff0a566e5b53832ab18ae00f3f46271e3333
Baibhav Pradhan <baibhavprdn371@gmail.com>
2019-08-06 14:33:55 +0545

    Basic app working

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -3,8 +3,22 @@ function onDeviceReady() {
    angular.bootstrap(document.body, ['formbuilder']);
 }
 
-function openDatabase(db) {
+function configRoutes($stateProvider, $urlRouterProvider) {
+   $stateProvider
+      .state('formbuilder', {
+         url: '/formbuilder',
+         templateUrl: 'views/formbuilder.html',
+         controller: 'BuilderCtrl'
+      })
+      .state('formviewer', {
+         url: '/formviewer',
+         templateUrl: 'views/formviewer.html',
+         controller: 'BuilderCtrl'
+      });
+   $urlRouterProvider.otherwise('/formbuilder');
+}
 
+function openDatabase() {
    function successcb() {
       console.log('Database opened successfully');
    }
@@ -14,43 +28,79 @@ function openDatabase(db) {
    }
 
    var dbObj = window.sqlitePlugin.openDatabase({
-      name: 'formBuilder.db',
+      name: 'formDb.db',
       location: 'default'
    }, successcb, errorcb);
    dbObj.transaction(function (tx) {
-      tx.executeSql('CREATE TABLE controlDefinitions (id integer primary key, Name text, Type text)', [], successcb, errorcb);
+      tx.executeSql('CREATE TABLE IF NOT EXISTS controlDefinitions (Name text primary key, Type text)', [], function () {
+         console.log('Table created successfully');
+      }, function (error) {
+         console.log('Failed to create table', JSON.stringify(error));
+      });
    });
-   db(dbObj);
+   return dbObj;
 }
 
 function insertToDb(ctrlName, ctrlType, dbObj) {
-   // dbObj.transaction(function (tx) {
-   //    tx.executeSql('CREATE TABLE controlDefinitions (id integer primary key, Name text, Type text)', [], function () {
-   //       console.log('Table created successfully');
-   //    }, function (error) {
-   //       console.log('Failed to create table', JSON.stringify(error));
-   //    });
-   // });
+   dbObj.transaction(function (tx) {
+      tx.executeSql('INSERT INTO controlDefinitions VALUES (?1,?2)', [ctrlName, ctrlType]);
+   }, function (error) {
+      console.log('Transaction error:' + error.message);
+   }, function () {
+      console.log('Populated Database OK');
+      window.plugins.toast.show('Added to database successfully', 'short', 'bottom', function (a) {
+         console.log('toast success: ' + a);
+      }, function (b) {
+         alert('toast error: ' + b);
+      });
+   });
 }
 
-function ControlBuilderCtrlFun($scope) {
+function readDb(dbObj, getArray) {
+   dbObj.transaction(function (tx) {
+      tx.executeSql('SELECT * FROM controlDefinitions', [], function (tx, results) {
+         getArray(results.rows);
+      }, function (error) {
+         console.log('Transaction error:' + error.message);
+      });
+   });
+}
+
+function BuilderCtrlFun($scope) {
+   var dbObj = openDatabase();
+   var dbDataArray = [];
+   $scope.dbData = [];
+
    $scope.submitToFormView = function () {
+      insertToDb($scope.ctrlName, $scope.data.singleSelect, dbObj);
+   };
 
-      var db = function (dbObj) {
-         insertToDb($scope.ctrlName, $scope.ctrlType, dbObj);
-      };
-      openDatabase(db);
+   var getArray = function (resultsrows) {
+      dbDataArray = [];
+      var length = resultsrows.length,
+         i;
+      for (i = 0; i < length; i++) {
+         dbDataArray.push(resultsrows.item(i));
+      }
+      $scope.dbData = dbDataArray;
+      console.log($scope.dbData);
+      $scope.generateViewBuilder = true;
+   };
 
+   $scope.generateForm = function () {
+      readDb(dbObj, getArray);
    };
 }
 
 document.addEventListener('deviceready', onDeviceReady, false);
 var formbuilder = angular.module('formbuilder', ['ui.router']);
 
-formbuilder.controller('ControlBuilderCtrl', ['$scope', function ($scope) {
-   ControlBuilderCtrlFun($scope);
+formbuilder.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+   configRoutes($stateProvider, $urlRouterProvider);
 }]);
-
+formbuilder.controller('BuilderCtrl', ['$scope', function ($scope) {
+   BuilderCtrlFun($scope);
+}]);
 document.addEventListener('DOMContentLoaded', function () {
    M.AutoInit();
 });
--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -7,10 +7,19 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "SQLitePlugin"
       ]
+    },
+    {
+      "id": "cordova-plugin-x-toast.Toast",
+      "file": "plugins/cordova-plugin-x-toast/www/Toast.js",
+      "pluginId": "cordova-plugin-x-toast",
+      "clobbers": [
+        "window.plugins.toast"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.4",
-    "cordova-sqlite-storage": "3.2.1"
+    "cordova-sqlite-storage": "3.2.1",
+    "cordova-plugin-x-toast": "2.7.2"
   };
 });
